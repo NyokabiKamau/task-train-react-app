446f60d490bc6ef0cf5e4a28fe3cc9211168566d
NyokabiKamau <nyokabikamau2014@gmail.com>
2023-03-21 20:45:43 +0300

    edited readme

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -5,7 +5,11 @@ function Header() {
 
     return (
         <div>
-            <h1 id="title"><span id="title_start">Minx'</span><span id="title_middle">s</span><span id="title_end"> Todos</span></h1>
+            <h1 id="title">
+                <span id="title_start">Minx'</span>
+                <span id="title_middle">s</span>
+                <span id="title_end"> Todos</span>
+            </h1>
         </div>
     )
 
