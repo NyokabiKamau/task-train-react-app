6551c925f561303d639b36cc8bcc5ad6d8fcc7bd
NyokabiKamau <nyokabikamau2014@gmail.com>
2023-03-21 14:46:29 +0300

    added a header

--- src/components/app/App.js
+++ src/components/app/App.js
@@ -1,9 +1,10 @@
-import logo from './logo.svg';
+import Header from '../header/header';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
+      <Header/>
     </div>
   );
 }
---
+++ src/components/header/header.js
@@ -0,0 +1,14 @@
+import React from "react";
+import './header.css'
+
+function Header() {
+
+    return (
+        <div>
+            <h1 id="title"><span id="title_start">Minx'</span><span id="title_middle">s</span><span id="title_end"> Todos</span></h1>
+        </div>
+    )
+
+}
+
+export default Header
