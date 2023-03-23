c3934b67d0c3e098c32327a13b4508773e0850af
NyokabiKamau <nyokabikamau2014@gmail.com>
2023-03-23 15:08:08 +0300

    done with view all todos

--- src/components/todo-card/todo-card.js
+++ src/components/todo-card/todo-card.js
@@ -5,10 +5,12 @@ function TodoCard({title, description, status, priority}){
     return (
         <ul className="cards">
             <li className="card">
-                <h4>{title}</h4>
+                <h3>{title}</h3>
                 <p>{description}</p>
-                <p>{status} {priority}</p>
+                <h4>Priority: {priority}</h4>
+                <h4>Status: {status}</h4>
                 <button>Delete</button>
+                <button>Edit</button>
             </li>
         </ul>
     )
