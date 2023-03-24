92cd507399a9077be771dc9a522cd2618a1cddf9
NyokabiKamau <nyokabikamau2014@gmail.com>
2023-03-24 04:44:05 +0300

    edited header styling

--- src/components/header/header.js
+++ src/components/header/header.js
@@ -1,8 +1,15 @@
 import React from "react";
 import './header.css'
 import { GoTasklist } from 'react-icons/go'
+import {Link} from 'react-router-dom'
 
-function Header() {
+function Header({token, onLogout}) {
+
+    function handleLogout() {
+        fetch("/logout", {
+          method: "DELETE",
+        }).then(() => onLogout());
+      }
 
     return (
         <div>
@@ -12,6 +19,14 @@ function Header() {
                 <span id="title_end"> Todos</span>
                 <span id="icon"> <GoTasklist/></span>
             </h1>
+            {token ? (
+                <div>
+                    <p>Welcome !</p>
+                    <button id="logout" onClick={handleLogout}>Logout</button>
+                </div>
+            ) : (
+                <Link to="/login"><button id="login" >Login</button></Link>
+            )}
         </div>
     )
 
